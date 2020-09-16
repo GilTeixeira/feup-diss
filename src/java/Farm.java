2b749a429bc6c0175caaa9f1ef8ee37bbfbfdb36
GilTeixeira <up201505735@fe.up.pt>
2020-09-16 18:48:39 +0100

    Add test Java files

---
+++ src/java/Farm.java
@@ -0,0 +1,8 @@
+public class Farm {
+    Puppy p = new Puppy("tommy");
+
+    int getPuppyAge() {
+        return p.getAge();
+    }
+
+}
--- src/java/Puppy.java
+++ src/java/Puppy.java
@@ -1,32 +1,34 @@
 public class Puppy {
-   int puppyAge;
+    public int puppyAge;
 
-   public Puppy(String name) {
-      // This constructor has one parameter, name.
-      if(true)
-      	System.out.println("Name chosen is :" + name );
-   }
+    public Puppy(String name) {
+        // This constructor has one parameter, name.
+        if (true)
+            System.out.println("Name chosen is :" + name);
 
-   public void setAge( int age ) {
-      puppyAge = age;
-   }
+    }
 
-   public int getAge( ) {
-      System.out.println("Puppy's age is :" + puppyAge );
-      return puppyAge;
-   }
 
-   public static void main(String []args) {
-      /* Object creation */
-      Puppy myPuppy = new Puppy( "tommy" );
+    public void setAge(int age) {
+        puppyAge = age;
+    }
 
-      /* Call class method to set puppy's age */
-      myPuppy.setAge( 2 );
 
-      /* Call another class method to get puppy's age */
-      myPuppy.getAge( );
+    public int getAge() {
+        System.out.println("Puppy's age is :" + puppyAge);
+        return puppyAge;
+    }
 
-      /* You can access instance variable as follows as well */
-      System.out.println("Variable Value :" + myPuppy.puppyAge );
-   }
+
+    public static void main(String[] args) {
+        /* Object creation */
+        Puppy myPuppy = new Puppy("tommy");
+        /* Call class method to set puppy's age */
+        myPuppy.setAge(2);
+        /* Call another class method to get puppy's age */
+        myPuppy.getAge();
+        /* You can access instance variable as follows as well */
+        System.out.println("Variable Value :" + myPuppy.puppyAge);
+    }
+
 }
