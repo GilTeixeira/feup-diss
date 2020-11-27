a4de6fe3b0b6be0d8608871ddd433ffd7006f252
Gil Teixeira <up201505735@fe.up.pt>
2020-11-27 22:22:33 +0000

    Metrics now handle when project does not have the full classpath

---
+++ MetricsCalculators/LARA/run.sh
@@ -0,0 +1,4 @@
+clava /home/gildt/Desktop/feup-diss/lara/Interface.lara -p /home/gildt/Desktop/feup-diss/MetricsCalculators/test/cpp -ncg -cl -o /home/gildt/Desktop/feup-diss -thd 4 -s -nci -pi -of woven
+
+# Parse results
+npm start --prefix ../parser/
